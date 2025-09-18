1308af46600ce6fe44ae255af666774e944de2d0
Joaco8907 <afscaiser@gmail.com>
2025-09-18 20:19:35 -0300

    descomente la primera intanciacion

--- Program.cs
+++ Program.cs
@@ -3,11 +3,13 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //Instancia_de_aplicacion inst_app = new Instancia_de_aplicacion("joaquin", "1", "Windows", 
-            //    0,"joaquin", "lautaro");
 
-            //inst_app.cambiarEstado();
+        {
+
+            Instancia_de_aplicacion inst_app = new Instancia_de_aplicacion("joaquin", "1", "Windows", 
+                0,"joaquin", "lautaro");
+
+            inst_app.cambiarEstado();
 
 
             Instancia_de_proceso inst_proceso = new Instancia_de_proceso("joaquin", "1", "Windows",
